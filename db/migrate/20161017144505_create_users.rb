e8adbdb534b5bfc32e9f6c1d508d3e7a4062c2f8
Nestor Alvarez <grimreaper1169@gmail.com>
2016-10-17 14:51:14 -0700

    Refactor our base migration file for User class to include a null:false clause inside timestamps. All other limitations kept intact.

--- db/migrate/20161017144505_create_users.rb
+++ db/migrate/20161017144505_create_users.rb
@@ -1,4 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+
+    create_table :users do |t|
+      t.string :username, { limit: 128 }
+      t.string :email, { limit: 128 }
+      t.string :hashed_password, { limit: 64 }
+
+      t.timestamps(null: false)
+    end
+
   end
 end
