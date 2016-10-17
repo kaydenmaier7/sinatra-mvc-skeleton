50e97ca01a98e0e3c448e682fef0019e88cbcf2c
Nestor Alvarez <grimreaper1169@gmail.com>
2016-10-17 15:45:15 -0700

    Files are simply renamed from CURRENT_NAME_ + controller.rb

---
+++ app/controllers/users.rb
@@ -0,0 +1,32 @@
+# USERS NEW
+get '/users/new' do
+  @user = User.new
+  erb :'users/new'
+end
+
+# USERS CREATE
+post '/users' do
+
+  if params[:password_confirmation] == params[:user][:password]
+    @user = User.new(params[:user])
+
+    if @user.save
+      session[:id] = @user.id
+      redirect "/users/#{@user.id}"
+    else
+      @errors = @user.errors.full_messages
+      erb :'users/new'
+    end
+
+  else
+    @errors = ["Passwords do not match!"]
+    erb :'users/new'
+  end
+
+end
+
+# USERS SHOW
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :'users/show'
+end
