9eaf6aa170c33fdd6441c3d6e36c992dcb254ed6
Nestor Alvarez <grimreaper1169@gmail.com>
2016-10-18 16:44:17 -0700

    Create a seeds file because I have created the same user so many times. Also a 42 slide quiz isn't fun to take.

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+User.create(username: 'helocat101', email: 'grimreaper1169@gmail.com', password: 'pass')
+Assessment.create(user_id: 1, name: 'Persuasion', key: '2754615a-d038-491e-91bf-a4829ea28fdd')
