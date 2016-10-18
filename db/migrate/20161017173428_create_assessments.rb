74b8f477dd1f82edb1132a659d09986fd103fb85
Nestor Alvarez <grimreaper1169@gmail.com>
2016-10-17 17:39:24 -0700

    Add things to create the assessments database.

--- db/migrate/20161017173428_create_assessments.rb
+++ db/migrate/20161017173428_create_assessments.rb
@@ -1,4 +1,11 @@
 class CreateAssessments < ActiveRecord::Migration
   def change
+    create_table :assessments do |t|
+      t.integer :user_id
+      t.string :name, { limit: 128 }
+      t.string :key, { limit: 64 }
+
+      t.timestamps(null: false)
+    end
   end
 end
