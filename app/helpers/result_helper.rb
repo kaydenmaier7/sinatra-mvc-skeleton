6c131e4e9f4a1fd69233ad80b3f20508bfff745a
Nestor Alvarez <grimreaper1169@gmail.com>
2016-10-18 12:13:17 -0700

    Create several methods, including two helper methods to help with the algorithm process.

--- app/helpers/result_helper.rb
+++ app/helpers/result_helper.rb
@@ -1,5 +1,7 @@
 helpers do
 
+  MAX_SCORE = 100;
+
   def assessment
     if logged_in?
       @assessment = Assessment.find_by(user_id: @user.id)
@@ -8,6 +10,33 @@ helpers do
 
   def completed_at
     assessment.updated_at.strftime('%b-%d-%Y')
+  end
+
+
+  def shorten(results)
+    shortened_results = {}
+    results['personality_types'].each do |personality|
+      shortened_results[personality['personality_type']['name']] = personality['score']
+    end
+    return shortened_results
+  end
+
+  def rate_short_results(results_arr)
+
+    return MAX_SCORE - 1
+
+  end
+
+  def traitify_results
+
+    access = User.traitify_access
+
+    results = access.find_results(assessment.key)
+
+    short_results = shorten(results)
+
+    rate_short_results(short_results)
+
   end
 
 end
