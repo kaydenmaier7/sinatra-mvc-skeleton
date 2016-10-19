f7dc4a852448d736837d3bea873a933a5bdb7ca4
Nestor Alvarez <grimreaper1169@gmail.com>
2016-10-18 17:25:04 -0700

    Create a default seed file for testing purposes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 User.create(username: 'helocat101', email: 'grimreaper1169@gmail.com', password: 'pass')
 Assessment.create(user_id: 1, name: 'Persuasion', key: '2754615a-d038-491e-91bf-a4829ea28fdd')
+Score.create(email: 'grimreaper1169@gmail.com', score: 88.67)
+Premium.create(username: 'admin', email: 'developer@devbootcamp.com')
