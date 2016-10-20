f68fac4a8e78f1601170a0243864aef0f0986ec5
Nestor Alvarez <grimreaper1169@gmail.com>
2016-10-20 11:34:32 -0700

    Add a method for personal use to see the top scorers.

---
+++ test.rb
@@ -0,0 +1,19 @@
+def database_pretty(arr)
+
+  sorted_results = arr.sort_by { |item| [item.score ? 0 : 1, item.score || 0] }
+
+  sorted_results.reverse!
+
+  sorted_results.each do |res|
+
+    if res.score
+      user = User.find(res.id)
+      p user.username + ' got a ' + res.score.to_s
+      p user.email
+      p "*" * 20
+    end
+
+  end
+
+end
+
