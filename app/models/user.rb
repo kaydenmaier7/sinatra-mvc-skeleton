ed718b7cb3f8fda9b8aae5427a953be0e2607144
tereg <teregalla@gmail.com>
2016-11-04 15:47:28 -0700

    Delete validates password from users model and add password validation to users create route

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ActiveRecord::Base
   validates_presence_of :last_name
   validates_presence_of :username
   validates_uniqueness_of :username
-  validates_presence_of :password
   validates_presence_of :email
   validates_uniqueness_of :email
   
